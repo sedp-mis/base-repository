f28555ff655c8501b5d2989f678da07f1029d277
markii1607 <mjbundalian61@gmail.com>
2016-07-19 11:47:30 +0800

    add testFetch test

--- tests/BaseRepositoryEloquentTest.php
+++ tests/BaseRepositoryEloquentTest.php
@@ -15,6 +15,8 @@ class BaseRepositoryEloquentTest extends TestCase
         $this->pdoExec("
             CREATE TABLE IF NOT EXISTS {$table} (
               `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+              `username` varchar(255) NULL COMMENT '',
+              `password` varchar(255) NULL COMMENT '',
               `name` varchar(255) NOT NULL COMMENT '',
               `xp` int(10) unsigned DEFAULT NULL COMMENT '',
               `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
@@ -29,6 +31,8 @@ class BaseRepositoryEloquentTest extends TestCase
     public function testShouldCreateStoreFindAndUpdateModel()
     {
         $spy = $this->repo->create([
+            'username' => 'ajcastro',
+            'password' => 'password',
             'name' => 'arjon',
             'xp' => 99
         ]);
@@ -45,5 +49,106 @@ class BaseRepositoryEloquentTest extends TestCase
 
         $updatedSpy = $this->repo->find($storedSpy->id);
         $this->assertEquals($updatedSpy->getAttributes(), $storedSpy->getAttributes());
+    }
+
+    public function testShouldFetchWithGivenAttributes()
+    {
+        $spies = [
+            [
+                'username' => 'markii1607',
+                'password' => 'secret',
+                'name' => 'mark',
+                'xp' => 99
+            ],
+            [
+                'username' => 'katbritanico',
+                'password' => 'secret',
+                'name' => 'katrina',
+                'xp' => 99
+            ],
+            [
+                'username' => 'janelagatuz',
+                'password' => 'secret',
+                'name' => 'janelle',
+                'xp' => 99
+            ],
+            [
+                'username' => 'jkmendez',
+                'password' => 'secret',
+                'name' => 'ken',
+                'xp' => 99
+            ],
+            [
+                'username' => 'guyabani',
+                'password' => 'secret',
+                'name' => 'giovani',
+                'xp' => 99
+            ],
+            [
+                'username' => 'ja9',
+                'password' => 'secret',
+                'name' => 'janine',
+                'xp' => 99
+            ],
+            [
+                'username' => 'aceruser22',
+                'password' => 'secret',
+                'name' => 'ace',
+                'xp' => 99
+            ],
+            [
+                'username' => 'jmoane',
+                'password' => 'secret',
+                'name' => 'jessa',
+                'xp' => 99
+            ],
+            [
+                'username' => 'cklucido',
+                'password' => 'secret',
+                'name' => 'kaye',
+                'xp' => 99
+            ],
+            [
+                'username' => 'jpintor',
+                'password' => 'secret',
+                'name' => 'jazarr',
+                'xp' => 99
+            ],
+            [
+                'username' => 'kbagasbas',
+                'password' => 'secret',
+                'name' => 'karen',
+                'xp' => 99
+            ],
+            [
+                'username' => 'tinejoy',
+                'password' => 'secret',
+                'name' => 'tine',
+                'xp' => 99
+            ],
+            [
+                'username' => 'rodabby',
+                'password' => 'secret',
+                'name' => 'abby',
+                'xp' => 99
+            ]
+        ];
+
+        $attributes = [
+            'username',
+            'name'
+        ];
+
+        foreach ($spies as $spy)
+        {
+            $resultSpy = $this->repo->create($spy);
+        }
+
+        $fetchSpies = $this->repo->fetch($attributes, [], [], null, 0);
+
+        foreach ($fetchSpies as $fetchSpy) {
+            $this->assertEquals(array_keys($fetchSpy->getAttributes()), $attributes);
+        }
+        // dd($this->repo->fetch()->toArray());
     }
 }
