29a07c6439b6e2a430d6c70c39528d234bb28787
markii1607 <mjbundalian61@gmail.com>
2016-07-30 01:53:35 +0800

    add eagerLoadRelation one to one for spy and target

--- test_classes/Spy.php
+++ test_classes/Spy.php
@@ -5,4 +5,9 @@ use Illuminate\Database\Eloquent\Model;
 class Spy extends Model
 {
     protected $guarded = [];    
+
+    public function target()
+    {
+      return $this->hasOne('Target', 'spy_id');
+    }
 }
