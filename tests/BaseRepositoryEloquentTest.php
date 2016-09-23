7fb90f5f6934dd3cb1bae448518241a40955a700
Arjon Jason Castro <ajcastro29@gmail.com>
2016-09-23 16:50:39 +0800

    Remove tests for deprecated methods. Use artisan migrate command.

--- tests/BaseRepositoryEloquentTest.php
+++ tests/BaseRepositoryEloquentTest.php
@@ -11,39 +11,18 @@ class BaseRepositoryEloquentTest extends TestCase
     {
         parent::setUp();
 
-        $table = "`{$this->db['database']}`.`spies`";
+        Artisan::call('migrate');
 
-        $this->pdoExec("DROP TABLE IF EXISTS {$table}");
-        
-        $this->pdoExec("
-            CREATE TABLE IF NOT EXISTS {$table} (
-              `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-              `username` varchar(255) NULL COMMENT '',
-              `password` varchar(255) NULL COMMENT '',
-              `name` varchar(255) NOT NULL COMMENT '',
-              `xp` int(10) unsigned DEFAULT NULL COMMENT '',
-              `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-              `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-              PRIMARY KEY (`id`)
-            ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
-        ");
-
-        $table = "`{$this->db['database']}`.`targets`";
-
-        $this->pdoExec("DROP TABLE IF EXISTS {$table}");
-        
-        $this->pdoExec("
-            CREATE TABLE IF NOT EXISTS {$table} (
-              `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-              `spy_id` int(10) unsigned NULL,
-              `name` varchar(255) NOT NULL COMMENT '',
-              `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-              `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-              PRIMARY KEY (`id`)
-            ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
-        ");
+        DB::beginTransaction();
 
         $this->repo = new SpyRepositoryEloquent(new Spy);
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+
+        DB::rollback();
     }
 
     public function testShouldCreateStoreFindAndUpdateModel()
@@ -167,137 +146,6 @@ class BaseRepositoryEloquentTest extends TestCase
         }
     }
 
-    public function testFetchFiltersUsingEquals()
-    {
-        $this->seedDb();
-
-        // Test `=` with single value
-        $spies = $this->repo->fetch(null, ['xp' => [
-            '=' => 352
-        ]]);
-
-        $this->assertEquals(1, $spies->count());
-
-        // Test `=` with array of values
-        $spies = $this->repo->fetch(null, ['xp' => [
-            '=' => [352]
-        ]]);
-
-        $this->assertEquals(1, $spies->count());
-
-        $spies = $this->repo->fetch(null, ['xp' => [
-            '=' => [352, 57]
-        ]]);
-
-        $this->assertEquals(2, $spies->count());
-
-        $spies = $this->repo->fetch(null, ['xp' => [
-            '=' => [352, 57, 172]
-        ]]);
-
-        $this->assertEquals(3, $spies->count());
-    }
-
-    public function testFetchFiltersUsingNotEquals()
-    {
-        $this->seedDb();
-
-        // Test `!=` with single value
-        $spies = $this->repo->fetch(null, ['xp' => [
-            '!=' => 352
-        ]]);
-
-        $this->assertEquals(2, $spies->count());
-
-        // Test `!=` with array of values
-        $spies = $this->repo->fetch(null, ['xp' => [
-            '!=' => [352]
-        ]]);
-
-        $this->assertEquals(2, $spies->count());
-
-        $spies = $this->repo->fetch(null, ['xp' => [
-            '!=' => [352, 57]
-        ]]);
-
-        $this->assertEquals(1, $spies->count());
-
-        $spies = $this->repo->fetch(null, ['xp' => [
-            '!=' => [352, 57, 172]
-        ]]);
-
-        $this->assertEquals(0, $spies->count());
-    }
-
-    public function testFetchFiltersUsingGreaterThan()
-    {
-        $this->seedDb();
-
-        // Test `>`
-        $spies = $this->repo->fetch(null, ['xp' => [
-            '>' => [100]
-        ]]);
-
-        $this->assertEquals(2, $spies->count());
-    }
-
-    public function testFetchFiltersUsingLessThan()
-    {
-        $this->seedDb();
-
-        // Test `<`
-        $spies = $this->repo->fetch(null, ['xp' => [
-            '<' => [100]
-        ]]);
-
-        $this->assertEquals(1, $spies->count());
-    }
-
-    public function testFetchFiltersUsingGreaterThanOrEqual()
-    {
-        $this->seedDb();
-
-        // Test `>=`
-        $spies = $this->repo->fetch(null, ['xp'=> [
-            '>=' => [180]
-        ]]);
-
-        $this->assertEquals(1, $spies->count());
-    }
-
-    public function testFetchFiltersUsingLessThanOrEqual()
-    {
-        $this->seedDb();
-        
-        // Test `<=`
-        $spies = $this->repo->fetch(null, ['xp'=> [
-            '<=' => [172]
-        ]]);
-
-        $this->assertEquals(2, $spies->count());
-    }
-
-    public function testFetchSortAscAndDesc()
-    {
-        $this->seedDb();
-
-        // Test for name in ascending order
-        $spies=$this->repo->fetch(null, null, [
-            'name' => 'asc'
-        ]);
-
-        $this->assertEquals($spies->first()->name, "janelle");
-        $this->assertEquals($spies->last()->name, "mark");
-
-        // Test for xp in descending order
-        $spies=$this->repo->fetch(null, null, [
-            'xp' => 'desc'
-        ]);
-
-        $this->assertEquals($spies->first()->xp, 352);
-        $this->assertEquals($spies->last()->xp, 57);
-    }
-
     public function seedForTestingMultipleSort()
     {
         $spies = [
@@ -319,137 +167,6 @@ class BaseRepositoryEloquentTest extends TestCase
         foreach ($spies as $spy) {
             $this->repo->create($spy);
         }
-    }
-
-    public function testFetchShouldSortUsingMultiple()
-    {
-        $this->seedForTestingMultipleSort();
-
-        $spies = $this->repo->fetch(null, null, [
-            'name' => 'asc',
-            'xp'   => 'desc'
-        ]);
-
-        $this->assertEquals($spies->first()->id, 3);
-        $this->assertEquals($spies->last()->id, 1);
-    }
-
-    public function testFetchLimitAndSkip()
-    {
-        $this->seedDb();
-
-        // Test with limit of 2
-        $spies=$this->repo->fetch(null, null, null, 2);
-
-        $this->assertTrue(count($spies)==2, "Spies collected: ".count($spies));
-
-        // Test with limit 1
-        $spies=$this->repo->fetch(null, null, null, 1);
-
-        $this->assertTrue(count($spies)==1, "Spies collected: ".count($spies));
-
-        // Test with limit 2 offset 1
-        $spies=$this->repo->fetch(null, null, null, 2, 1);
-
-        $this->assertTrue(count($spies)==2, "Spies collected: ".count($spies));
-        $this->assertEquals($spies->first()->id, 2);
-
-        // Test with limit 1 offset 2
-        $spies=$this->repo->fetch(null, null, null, 1, 2);
-
-        $this->assertTrue(count($spies)==1, "Spies collected: ".count($spies));
-        $this->assertEquals($spies->first()->id, 3);
-    }
-
-    public function testFetchForAllParameters()
-    {
-        $this->seedDb();
-
-        $attributes = [
-            'username',
-            'name'
-        ];
-
-        $filters = [
-            'xp' => [
-                '=' => [
-                    352, 
-                    57
-                ]
-            ]
-        ];
-
-        $sort = [
-            'name' => 'desc'
-        ];
-
-        $spies=$this->repo->fetch($attributes, $filters, $sort, 1, 1);
-
-        $this->assertTrue(count($spies) == 1, "Spies Count: ".count($spies));
-
-        foreach ($spies as $spy) {
-            $this->assertEquals(array_keys($spy->getAttributes()), $attributes);
-        }
-
-        $this->assertEquals($spies->first()->name, "janelle");
-    }
-
-    public function testPagination()
-    {
-        $this->seedDb();
-        $this->seedMoreForPaginate();
-
-        $attributes = [
-            'username',
-            'name'
-        ];
-
-        $filters = [
-            'xp' => [
-                '=' => [
-                    352, 
-                    57,
-                    86,
-                    145,
-                    41,
-                    321,
-                    512,
-                    91
-                ]
-            ]
-        ];
-
-        $sort = [
-            'name' => 'desc'
-        ];
-
-        // page 1, 3 data per page
-        $spies = $this->repo->paginate($attributes, $filters, $sort, 3, 1);
-
-        $this->assertTrue(count($spies) == 3, "Spies collected: ".count($spies));
-        $this->assertEquals($spies[0]->getAttributes()['name'], "tine");
-        $this->assertEquals($spies[count($spies)-1]->getAttributes()['name'], "kaye");
-
-        // page 3, 3 data per page
-        $spies = $this->repo->paginate($attributes, $filters, $sort, 3, 3);
-
-        $this->assertTrue(count($spies) == 2, "Spies collected: ".count($spies));
-        $this->assertEquals($spies[0]->getAttributes()['name'], "janelle");
-        $this->assertEquals($spies[count($spies)-1]->getAttributes()['name'], "giovani");
-
-        // page 2, 4 data per page
-        $spies = $this->repo->paginate($attributes, $filters, $sort, 4, 2);
-
-        $this->assertTrue(count($spies) == 4, "Spies collected: ".count($spies));
-        $this->assertEquals($spies[0]->getAttributes()['name'], "karen");
-        $this->assertEquals($spies[count($spies)-1]->getAttributes()['name'], "giovani");
-
-        // page 2, 2 data per page
-        $spies = $this->repo->paginate($attributes, $filters, $sort, 2, 2);
-
-        $this->assertTrue(count($spies) == 2, "Spies collected: ".count($spies));
-        $this->assertEquals($spies[0]->getAttributes()['name'], "kaye");
-        $this->assertEquals($spies[count($spies)-1]->getAttributes()['name'], "katrina");
     }
 
     public function seedWithTarget()
