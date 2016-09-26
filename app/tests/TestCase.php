c1a85affa35313b0bcccd8bf65aa7de903b040c9
Arjon Jason Castro <ajcastro29@gmail.com>
2016-09-26 11:42:15 +0800

    Remove unneccessary folders and files.

--- app/tests/TestCase.php
+++ app/tests/TestCase.php
@@ -2,17 +2,6 @@
 
 class TestCase extends Illuminate\Foundation\Testing\TestCase
 {
-	protected $db = [
-        'driver'    => 'mysql',
-        'host'      => 'localhost',
-        'database'  => 'base_repo_test_db',
-        'username'  => 'homestead',
-        'password'  => 'secret',
-        'charset'   => 'utf8',
-        'collation' => 'utf8_unicode_ci',
-        'prefix'    => '',
-    ];
-
 	/**
 	 * Creates the application.
 	 *
@@ -31,21 +20,5 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase
     {
         parent::setUp();
 
-        $this->setupDatabase();
-    }
-
-    public function setupDatabase()
-    {
-        $this->pdoExec('create database if not exists '.$this->db['database']);
-    }
-
-    public function pdoExec($sql)
-    {
-        $driver   = $this->db['driver'];
-        $host     = $this->db['host'];
-        $username = $this->db['username'];
-        $password = $this->db['password'];
-        $pdo = new PDO("{$driver}:host={$host}", "{$username}", "{$password}");
-        $pdo->exec($sql);
     }
 }
