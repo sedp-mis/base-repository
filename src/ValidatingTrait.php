d3fb8995f10c55de92010523da2cb258287c16ac
Arjon Jason Castro <ajcastro29@gmail.com>
2016-09-27 11:21:28 +0800

    Add docblocks

--- src/ValidatingTrait.php
+++ src/ValidatingTrait.php
@@ -2,23 +2,48 @@
 
 namespace SedpMis\BaseRepository;
 
+/**
+ * Validating trait to get rules.
+ */
 trait ValidatingTrait
 {
+    /**
+     * Return the default rules for validation.
+     *
+     * @return array
+     */
     public function defaultRules()
     {
         return property_exists($this, 'rules') ? $this->rules : [];
     }
 
+    /**
+     * Return the create rules for validation.
+     *
+     * @return array
+     */
     public function createRules()
     {
         return $this->defaultRules();
     }
 
+    /**
+     * Return the update rules for validation.
+     *
+     * @return array
+     */
     public function updateRules()
     {
         return $this->defaultRules();
     }
 
+    /**
+     * Return the rules for validation.
+     *
+     * @param  string|array  $key
+     * @param  string  $operation
+     * @return array
+     */
     public function rules($key = null, $operation = null)
     {
         $ruleName = $operation ? "{$operation}Rules" : 'defaultRules';
